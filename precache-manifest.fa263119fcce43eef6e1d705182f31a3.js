cb53cf076baff54bd114d0fc12b79f59b5fba842
Harsh Gupta <harshgupta951114@gmail.com>
2020-06-02 14:05:19 +0530

    Updates

---
+++ precache-manifest.fa263119fcce43eef6e1d705182f31a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65026c14b93409ca0d562d5930d8dfca",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "69e0b4ea96a7929b3bad",
+    "url": "/login/static/css/main.969a7493.chunk.css"
+  },
+  {
+    "revision": "4659d2d8e3c01e98d7a0",
+    "url": "/login/static/js/2.7131f240.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/login/static/js/2.7131f240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e0b4ea96a7929b3bad",
+    "url": "/login/static/js/main.39f58b13.chunk.js"
+  },
+  {
+    "revision": "8b3fdd58800c03908777",
+    "url": "/login/static/js/runtime-main.975a3f31.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/airport-fuel-inventory/precache-manifest.9a8928e40199258d0b8805fac26106d6.js"
+  "/login/precache-manifest.fa263119fcce43eef6e1d705182f31a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/airport-fuel-inventory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/login/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
